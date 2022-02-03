535fa5a9cb8aa688b802ab4cbfe6a5090762b6de
hmcvay <hayleymcvay8@gmail.com>
2022-02-03 14:22:51 -0800

    WIP showing/hiding UI results. > > > Co-Authored By: Eric Crudup <briefcasemanx@gmail.com> > Co-Authored-By: Kevin Williams <kevin@k9w.org>

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import $ from 'jquery';
-
-// const d = new Date('Feb 2 2001');
+import './css/styles.css';
 
 const nameOfWeekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-// const day = d.getDay();
 
-// nameOfWeekday[day]; //string to give back to user
-
-// 2022-02-03
-// 2022/02/03
 $("#submitDate").click(function() {
   const userDate =  $("input#userDate").val().replace( new RegExp( '-', 'g'), "/");
   
-  //.replace(/-/g, '\/');
   console.log(userDate);
   $("#inputDate").text(userDate);
   const d = new Date(userDate);
   console.log(d);
-  const day = d.getDay();
-  console.log(day);
-  const result = nameOfWeekday[day];
-  $("#dayValue").text(result);
+  if ( isNaN(d) ) {
+    $("#error").show();
+    $("#error").text("ENTER A VALID DATE!!!");
+  } else {
+    const day = d.getDay();
+    // console.log(day);
+    const result = nameOfWeekday[day];
+    $("#result").show();
+    $("#dayValue").text(result);  
+  }
+  
 });
 
 
