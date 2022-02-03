674027fa2c1ab8674d34744a1baec1dce93d9f37
hmcvay <hayleymcvay8@gmail.com>
2022-02-02 16:36:20 -0800

    WIP: Add second test to display named day based on index number; got first good fail.
    Co-Authored-By: Kevin Williams <kevin@k9w.org>

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -6,4 +6,13 @@ describe('Date', () => {
     const d = new Date('Feb 2 2001');
     expect(d.getDay()).toEqual(5);
   });
+
+  test('should correctly display the named 3-letter weekday corresponding to the index number', () => {
+    const nameOfWeekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = d.getDay(5);
+    expect(nameOfWeekday[day]).toEqual('Friday');
+  });
+
 });
+
+  
